51067ce710f43d3b1d37bd60a33b943e6b980ed4
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-06 12:33:03 +0530

    remove all the radium setup and its media styling

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import './person.css';
-import Radium from 'radium';
 
 const person = (props) => {
-
-    const style = {
-        '@media(min-width: 500px': {
-            width: '450px'
-        } 
-    }
     return (
-    <div className="Person" style={style}>
+    <div className="Person">
         <p onClick={props.click}> I am {props.name} and I am {props.age} years old!! </p>
         <p> {props.children} </p>
         <input type="text" onChange={props.changed} value={props.name}/>
@@ -18,6 +11,6 @@ const person = (props) => {
 };
 
 
-export default Radium(person);
+export default person;
 
 
