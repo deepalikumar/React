8cba8bdb8175fb6dd1e8f1a010c32b179e065172
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-07 13:00:18 +0530

    added random error and wrapped person component to the errorBoundary class

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import styles from './App.css';
-import Person from './Person/Person.js';
+import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 
 class App extends Component {
@@ -70,12 +71,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person click={() => this.deletePersonHandler(index)}
-            name={person.name}
+            return <ErrorBoundary key={person.id} 
+            > <Person click={() => this.deletePersonHandler(index)}
+             name={person.name}
              age={person.age}
-             key={person.id} 
              changed={(event) => this.nameChangedHandler(event, person.id)}
-             /> 
+             /></ErrorBoundary> 
           })}
           
         </div> )
