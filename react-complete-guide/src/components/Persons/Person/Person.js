07857f0724d7c312c35ca32acd899b9c72f316c5
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-08 20:16:35 +0530

    proptypes uses for props checking and also use of the ref

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import classes from  './person.css';
 import withClass from '../../../hoc/withClass';
 import Aux from '../../../hoc/Aux';
@@ -15,20 +16,31 @@ class Person extends Component {
     
       componentDidMount() {
         console.log('[Person.js] inside componentDidMount()');
+        if( this.props.position === 0) {
+            this.inputElement.focus();
+        }
       }
     
     render(){
         console.log('[Person.js inside render' );
         return (
-            <Aux>    
+            <Aux> 
                 <p onClick={this.props.click}> I am {this.props.name} and I am {this.props.age} years old!! </p>
                 <p> {this.props.children} </p>
-                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+                <input
+                ref={(inp) => {this.inputElement = inp }} 
+                type="text" onChange={this.props.changed} value={this.props.name}/>
             </Aux> )
     }
 
 } 
 
+PropTypes.Person = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+}
 
 export default withClass(Person, classes.Person);
 
