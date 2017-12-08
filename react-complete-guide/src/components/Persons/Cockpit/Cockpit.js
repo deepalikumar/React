fef0366ead1fa4ad61c787de5ed81750e8fecabd
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-08 15:52:31 +0530

    created aux function to wrap the jsx code

--- react-complete-guide/src/components/Persons/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Persons/Cockpit/Cockpit.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './cockpit.css';
+import Aux from '../../../hoc/Aux';
 
 const cockpit = (props) => {
    const assigneDclasses = [];
-   let btnClass = ''
+   let btnClass = styles.button;
    if (props.showPersons) {
-       btnClass = styles.Red;
+       btnClass = [styles.button ,styles.Red ].join(' ');
    }
     if (props.persons.length <= 2){
       assigneDclasses.push(styles.red);
@@ -14,13 +15,12 @@ const cockpit = (props) => {
     if (props.persons.length <=1) {
       assigneDclasses.push(styles.bold);
     }
-    return (
-        <div className={styles.cockpit}>
-            <h1>{props.appTitle} </h1>
-                <p className={assigneDclasses.join(' ')}>This is really working !! </p>
-                 <button className={btnClass} onClick={props.clicked} > Toggle Persons </button>
-        </div>
-
+    return (   
+        <Aux>
+             <h1>{props.appTitle} </h1>
+             <p className={assigneDclasses.join(' ')}>This is really working !! </p>
+             <button className={btnClass} onClick={props.clicked} > Toggle Persons </button>
+        </Aux>
     );
 }
 
