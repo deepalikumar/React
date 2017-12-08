bfad774bddb8f0fd4d467b98e0cf3546ffd1f78f
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-08 19:12:37 +0530

    wrapping component into higher order component

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from  './person.css';
-import WithClass from '../../../hoc/withClass';
+import withClass from '../../../hoc/withClass';
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
   constructor(props) {
@@ -19,16 +20,16 @@ class Person extends Component {
     render(){
         console.log('[Person.js inside render' );
         return (
-            <WithClass classes={classes.Person}>    
+            <Aux>    
                 <p onClick={this.props.click}> I am {this.props.name} and I am {this.props.age} years old!! </p>
                 <p> {this.props.children} </p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
-            </WithClass> )
+            </Aux> )
     }
 
 } 
 
 
-export default Person;
+export default withClass(Person, classes.Person);
 
 
