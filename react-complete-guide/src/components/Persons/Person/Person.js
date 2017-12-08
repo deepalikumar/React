0b942e1e3aaa12b22453626dfdc193d4a95f341e
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-08 13:13:18 +0530

    component creation life cycle hooks

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import classes from  './person.css';
 
 class Person extends Component {
-    render() {
+  constructor(props) {
+        super(props);
+        console.log('[Person.js] inside constructor', props);
+      }
+    
+      componentWillMount(){
+        console.log('[Person.js] inside component will mount');
+      }
+    
+      componentDidMount() {
+        console.log('[Person.js] inside componentDidMount()');
+      }
+    
+    render(){
+        console.log('[Person.js inside render' );
         return (
             <div className={classes.Person}>    
                 <p onClick={this.props.click}> I am {this.props.name} and I am {this.props.age} years old!! </p>
