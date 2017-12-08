cef051537fde384006959e19a79ff2bbfefaecdb
deepaliprasad <deepaliprasad123@gmail.com>
2017-12-08 19:12:00 +0530

    higher order component which returns a function

--- react-complete-guide/src/hoc/withClass.js
+++ react-complete-guide/src/hoc/withClass.js
@@ -1,8 +1,10 @@
 import React from 'react';
-const withClass = (props) => (
-    <div className={props.classes}>
-        {props.children}
+const withClass = (WrappedComponent, className) => {
+    return (props) => (
+    <div className={className}>
+        <WrappedComponent  />
     </div>
-)
+    )
+}
 
 export default withClass;
